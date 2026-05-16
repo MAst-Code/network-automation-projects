a73bf36ca482a07173064960982e2d7a4628b29d
Marvin A <marvin441@icloud.com>
2026-05-16 23:41:45 +0100

    Modified file_processing, added documentation

--- Python Scripts/file_processing.py
+++ Python Scripts/file_processing.py
@@ -26,23 +26,34 @@
 # I thought it did it character by character, changing the script to use a for loop\
 # My code
 
-with open('devices.txt') as f:
-    f.readline()
+with open('devices.txt') as f:                # open file devices.txt in read only mode
+    f.readline()                              # Skip the first line of the file
 
-    devices_list = []
+    devices_list = []                         # variable to place our device content in
 
-    for line in f:
-        content = line.strip().split(':')
-        devices_list.append(content)
-print(devices_list)
+    for line in f:                            # iterate through each line of the file
+        content = line.strip().split(':')     # Remove whitespace and split values by ':' in the string
+        devices_list.append(content)          # Add the now list data into devices_list
+
+    print(devices_list)                       # Print nested lists of device attributes
+
+    for device in devices_list:               # Print the IP address of each device
+        print(f'Pinging {device[1]}')
+
 
 # Instructor code
 
 with open('devices.txt') as f:
     content = f.read().splitlines()
     # print(content)
-    device = list()
+    devices = list()
     for line in content[1:]:
-        device.append(line.split(':'))
-print(device)
+        devices.append(line.split(':'))
+
+    print(devices)
+
+    for device in devices:
+        print(f'Pinging {device[1]}')
+
+    
 
